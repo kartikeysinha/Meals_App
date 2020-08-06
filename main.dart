b94f494097123a5b9625f36a67290a401f9db699
Kartikey Sinha <52502376+kartikeysinha@users.noreply.github.com>
2020-08-07 04:05:30 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'widgets_screen/categories_screen.dart';
+import 'widgets_screen/meal_detail_screen.dart';
+import 'widgets_screen/meals_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Travel App',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+        accentColor: Colors.green,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        MealsScreen.routeName: (ctx) => MealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
+      },
+    );
+  }
+}
