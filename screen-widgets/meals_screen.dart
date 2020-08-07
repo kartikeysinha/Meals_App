be807544d6c43a7ee4174bd7df29bcda4f082071
Kartikey Sinha <52502376+kartikeysinha@users.noreply.github.com>
2020-08-07 20:23:34 +0530

    Update and rename meals_screen.dart to screen-widgets/meals_screen.dart

---
+++ screen-widgets/meals_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import '../Widgets/meal_item.dart';
+import '../models/meal.dart';
+
+class MealsScreen extends StatefulWidget {
+  static const routeName = '/meals';
+  final List<Meal> availableMeals;
+
+  MealsScreen(this.availableMeals);
+
+  @override
+  _MealsScreenState createState() => _MealsScreenState();
+}
+
+class _MealsScreenState extends State<MealsScreen> {
+  String categoryTitle;
+  List<Meal> displayedMeals;
+  var _loadedInitData = false;
+
+  @override
+  void initState() {
+    // ...
+    super.initState();
+  }
+
+@override
+  void didChangeDependencies() {
+    if (!_loadedInitData) {
+      final routeArgs =
+          ModalRoute.of(context).settings.arguments as Map<String, String>;
+      categoryTitle = routeArgs['title'];
+      final categoryId = routeArgs['id'];
+      displayedMeals = widget.availableMeals.where((meal) {
+        return meal.categories.contains(categoryId);
+      }).toList();
+      _loadedInitData = true;
+    }
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: displayedMeals[index].id,
+            title: displayedMeals[index].title,
+            imageUrl: displayedMeals[index].imageUrl,
+            duration: displayedMeals[index].duration,
+            complexity: displayedMeals[index].complexity,
+            affordability: displayedMeals[index].affordability,
+          );
+        },
+        itemCount: displayedMeals.length,
+      ),
+    );
+  }
+}
