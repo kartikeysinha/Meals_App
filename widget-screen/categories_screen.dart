4261d6f1020525c1a225c7c37c852977ba26ff5c
Kartikey Sinha <52502376+kartikeysinha@users.noreply.github.com>
2020-08-07 20:24:44 +0530

    Update and rename categories_screen.dart to widget-screen/categories_screen.dart

---
+++ widget-screen/categories_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+import '../Widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: EdgeInsets.all(MediaQuery.of(context).size.width / 25),
+      children: DUMMY_CATEGORIES
+          .map(
+            (catData) => CategoryItem(
+              catData.id,
+              catData.title,
+              catData.color,
+            ),
+          )
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: MediaQuery.of(context).size.width / 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: MediaQuery.of(context).size.width / 25,
+        mainAxisSpacing: MediaQuery.of(context).size.height / 50,
+      ),
+    );
+  }
+}
